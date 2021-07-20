5d3458d763eeb4e7d75223f33c4fd1bc8bca29b7
Parker Mojsiejenko <pmojo375@gmail.com>
2021-07-20 09:47:50 -0400

    Split out remaining features

--- plc.py
+++ plc.py
@@ -268,55 +268,43 @@ def main():
 
         # create empty data frame
         dfOut = pandas.DataFrame()
+        if not debug:
+            trend_tag(ip, tag)
+        else:
+            # use try block to allow user to exit endless loop via CTRL + C
+            try:
+                while True:
+                    # current date and time
+                    now = datetime.now()
+                    date_time = now.strftime("%m/%d/%Y %H:%M:%S")
 
-        # use try block to allow user to exit endless loop via CTRL + C
-        try:
-            while True:
-                # current date and time
-                now = datetime.now()
-                date_time = now.strftime("%m/%d/%Y %H:%M:%S")
-
-                if not debug:
-                    # read the tag from the PLC
-                    with LogixDriver(ip) as plc:
-                        result = plc.read(tag)
-
-                        # crawl through the result and store results
-                        data = crawl_and_format(
-                            result.value, 0, result.tag, {})
-
-                        # add the timestamp to the results
-                        data['timestamp'] = date_time
-
-                else:
                     # dummy data to test
                     dummy_data = {'Status': 0, 'RejectCode': 0, 'Model': {'Name': '', 'ModelNum': '', 'TallPart': False, 'ShortPart': False, 'UEF_Galv': False, 'UEF_Coated': False}, 'Barcode': '', 'Sta3000_HipotTestStatus': 0, 'Sta5000_LeakTestStatus': 0, 'Sta5000_EnclosureScrew1': {'Torque': 0.0, 'Angle': 0.0}, 'Sta5000_EnclosureScrew2': {'Torque': 0.0, 'Angle': 0.0}, 'Sta5000_EnclosureScrew3': {'Torque': 0.0, 'Angle': 0.0}, 'Sta5000_EnclosureScrew4': {'Torque': 0.0, 'Angle': 0.0}, 'Sta6000_HighVoltageTest': {'Status': 0, 'ContinuityTest': {'Status': '', 'Resistance': 0.0}, 'ACWTest': {'Status': '', 'Volts': 0.0, 'TotalAmps': 0.0, 'RealAmps': 0.0}, 'RunTest': {'Status': '', 'Volts': 0.0, 'Amps': 0.0, 'Watts': 0.0, 'RawVolts': 0.0, 'RawAmps': 0.0, 'RawWatts': 0.0, 'TestTime': 0.0, 'PowerFactor': 0.0}, 'PassedContinuity': False, 'PassedHiPot': False, 'PassedRun': False, 'VoltageInRange': False, 'CurrentInRange': False, 'PowerInRange': False}, 'Sta6000_LowVoltageTest': {'Status': 0, 'ContinuityTest': {'Status': '', 'Resistance': 0.0}, 'ACWTest': {'Status': '', 'Volts': 0.0, 'TotalAmps': 0.0, 'RealAmps': 0.0}, 'RunTest': {'Status': '', 'Volts': 0.0, 'Amps': 0.0, 'Watts': 0.0, 'RawVolts': 0.0, 'RawAmps': 0.0, 'RawWatts': 0.0, 'TestTime': 0.0, 'PowerFactor': 0.0}, 'PassedContinuity': False, 'PassedHiPot': False, 'PassedRun': False, 'VoltageInRange': False,
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        'CurrentInRange': False, 'PowerInRange': False}, 'Sta6000_LeakTestStatus': 0, 'Sta7000_TerminalPlateScrew': {'Torque': 0.0, 'Angle': 0.0}, 'Sta3000_Rework_Count': 0, 'Sta4000_UEFPlace_Status': 0, 'Sta5000_Rework_Status': 0, 'Sta6000_Lane_Status': 0, 'Sta2000_LoopNum': 0, 'Sta1000_ReleaseTime': {'Year': 0, 'Month': 0, 'Day': 0, 'Hour': 0, 'Minute': 0, 'Second': 0, 'Microsecond': 0}, 'Sta2000_ReleaseTime': {'Year': 0, 'Month': 0, 'Day': 0, 'Hour': 0, 'Minute': 0, 'Second': 0, 'Microsecond': 0}, 'Sta7000_UnloadTime': {'Year': 0, 'Month': 0, 'Day': 0, 'Hour': 0, 'Minute': 0, 'Second': 0, 'Microsecond': 0}, 'PalletNum': 0, 'TestRecord': {'Base': '', 'Matrix': '', 'DateTime': '', 'TestResults': [{'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}, {'Status': 0, 'Value': 0.0}], 'TestLane': 0, 'MasterFail': 0, 'FailTest': 0, 'ProdLine': 74}, 'Tracking': 0}
 
-                    data = {}
                     data = crawl_and_format(
-                        dummy_data, 0, 'PalletRecords[1]', data)
+                        dummy_data, 0, 'PalletRecords[1]', {})
 
                     # add the timestamp to the results
                     data['timestamp'] = date_time
 
-                # append data to the data frame
-                dfOut = dfOut.append(data, ignore_index=True)
+                    # append data to the data frame
+                    dfOut = dfOut.append(data, ignore_index=True)
 
-                # shift column 'timestamp' to first position
-                first_column = dfOut.pop('timestamp')
+                    # shift column 'timestamp' to first position
+                    first_column = dfOut.pop('timestamp')
 
-                # insert column using insert(position, column_name, first_column) function
-                dfOut.insert(0, 'timestamp', first_column)
+                    # insert column using insert(position, column_name, first_column) function
+                    dfOut.insert(0, 'timestamp', first_column)
 
-                # output to CSV file
-                dfOut.to_csv(out, index=False)
+                    # output to CSV file
+                    dfOut.to_csv(out, index=False)
 
-                # sleep a number of seconds set in the config file
-                time.sleep(streamtime)
+                    # sleep a number of seconds set in the config file
+                    time.sleep(streamtime)
 
-        except KeyboardInterrupt:
-            pass
+            except KeyboardInterrupt:
+                pass
 
     # monitor multiple tags set in a CSV file and write their results to another CSV file
     elif choice == 6:
@@ -367,54 +355,19 @@ def main():
                 # create empty data frame to aid in writing to CSV file
                 dfOut[i] = pandas.DataFrame()
 
-        # use a try block to allow the user to exit the endless loop via CTRL + C
-        try:
-            while True:
+        if not debug:
+            trend_tags(ip, file_name, splitout)
+        else:
+            # use a try block to allow the user to exit the endless loop via CTRL + C
+            try:
+                while True:
 
-                data = {}
+                    data = {}
 
-                # current date and time
-                now = datetime.now()
-                date_time = now.strftime("%m/%d/%Y %H:%M:%S")
+                    # current date and time
+                    now = datetime.now()
+                    date_time = now.strftime("%m/%d/%Y %H:%M:%S")
 
-                if not debug:
-                    with LogixDriver(ip) as plc:
-                        # outputs a list of results
-                        tagsRead = plc.read(*tags)
-
-                    # loop through the results and crawl through them to store
-                    for result in tagsRead:
-
-                        if splitout:
-                            data = {}
-
-                        data = crawl_and_format(
-                            result.value, 0, result.tag, data)
-
-                        # add the timestamp to the results
-                        data['timestamp'] = date_time
-
-                        if splitout:
-
-                            # append the results to an empty data frame
-                            dfOut[result.tag] = dfOut[result.tag].append(
-                                data, ignore_index=True)
-
-                            # shift column 'timestamp' to first position
-                            first_column = dfOut[result.tag].pop('timestamp')
-
-                            # insert column using insert(position, column_name, first_column) function
-                            dfOut[result.tag].insert(
-                                0, 'timestamp', first_column)
-
-                            out_name = result.tag.replace(".", "_") + '.csv'
-                            dfOut[result.tag].to_csv(out_name, index=False)
-
-                    if not splitout:
-                        # add the timestamp to the results
-                        data['timestamp'] = date_time
-
-                else:
                     # if in debug mode, add the time stamp to the results
                     data = {}
                     data['timestamp'] = date_time
@@ -448,28 +401,27 @@ def main():
                         print(
                             f"{Style.RESET_ALL}{Style.BRIGHT}{Fore.WHITE}{data}")
 
-                if not splitout:
-                    # append the results to an empty data frame
-                    dfOut = dfOut.append(data, ignore_index=True)
+                        # append the results to an empty data frame
+                        dfOut = dfOut.append(data, ignore_index=True)
 
-                    # shift column 'timestamp' to first position
-                    first_column = dfOut.pop('timestamp')
+                        # shift column 'timestamp' to first position
+                        first_column = dfOut.pop('timestamp')
 
-                    # insert column using insert(position, column_name, first_column) function
-                    dfOut.insert(0, 'timestamp', first_column)
+                        # insert column using insert(position, column_name, first_column) function
+                        dfOut.insert(0, 'timestamp', first_column)
 
-                    # output data to CSV file
-                    dfOut.to_csv(out, index=False)
+                        # output data to CSV file
+                        dfOut.to_csv(out, index=False)
 
-                # wait a number of seconds set in the config file
-                time.sleep(streamtime)
+                    # wait a number of seconds set in the config file
+                    time.sleep(streamtime)
 
-        except KeyboardInterrupt:
-            pass
+            except KeyboardInterrupt:
+                pass
 
-    # output a user prompt to inform them the program is over
-    print(f'{Style.RESET_ALL}{Style.BRIGHT}{Fore.YELLOW}')
-    input(f"Press any key to close window")
+        # output a user prompt to inform them the program is over
+        print(f'{Style.RESET_ALL}{Style.BRIGHT}{Fore.YELLOW}')
+        input(f"Press any key to close window")
 
 
 def read_tag(ip, tag):
@@ -527,6 +479,140 @@ def write_tags_from_CSV(csv, ip):
         for index, data in df.iterrows():
             # write to the PLC
             plc.write(data['tag'], cast(data['value']))
+
+
+def trend_tag(ip, tag, streamtime):
+    # use try block to allow user to exit endless loop via CTRL + C
+    try:
+        while True:
+            # current date and time
+            now = datetime.now()
+            date_time = now.strftime("%m/%d/%Y %H:%M:%S")
+
+            # read the tag from the PLC
+            with LogixDriver(ip) as plc:
+                result = plc.read(tag)
+
+                # crawl through the result and store results
+                data = crawl_and_format(
+                    result.value, 0, result.tag, {})
+
+                # add the timestamp to the results
+                data['timestamp'] = date_time
+
+            # append data to the data frame
+            dfOut = dfOut.append(data, ignore_index=True)
+
+            # shift column 'timestamp' to first position
+            first_column = dfOut.pop('timestamp')
+
+            # insert column using insert (position, column_name, first_column) function
+            dfOut.insert(0, 'timestamp', first_column)
+
+            # output to CSV file
+            dfOut.to_csv(out, index=False)
+
+            # sleep a number of seconds set in the config file
+            time.sleep(streamtime)
+    except KeyboardInterrupt:
+        pass
+
+def trend_tags(ip, csv, splitout, streamtime):
+
+    # use pandas to read tags from CSV file
+    df = pandas.read_csv(csv)
+
+    if not splitout:
+        # create empty data frame to aid in writing to CSV file
+        dfOut = pandas.DataFrame()
+
+    # create empty array to store the data
+    tags = []
+
+    # inform the user tags are being read
+    print(f"Reading tags\n")
+
+    indexes = {}
+
+    # create the header and an array of the tags
+    for index, data in df.iterrows():
+        tags.append(data['tag'])
+        indexes[data['tag']] = index
+
+    # print the results
+    print('Reading the following tags:')
+    print(f"{tags}\n")
+
+    if splitout:
+        dfOut = {}
+
+        for i in tags:
+            # create empty data frame to aid in writing to CSV file
+            dfOut[i] = pandas.DataFrame()
+
+    # use a try block to allow the user to exit the endless loop via CTRL + C
+    try:
+        while True:
+
+            data = {}
+
+            # current date and time
+            now = datetime.now()
+            date_time = now.strftime("%m/%d/%Y %H:%M:%S")
+
+            with LogixDriver(ip) as plc:
+                # outputs a list of results
+                tagsRead = plc.read(*tags)
+
+            # loop through the results and crawl through them to store
+            for result in tagsRead:
+
+                if splitout:
+                    data = {}
+
+                data = crawl_and_format(
+                    result.value, 0, result.tag, data)
+
+                # add the timestamp to the results
+                data['timestamp'] = date_time
+
+                if splitout:
+
+                    # append the results to an empty data frame
+                    dfOut[result.tag] = dfOut[result.tag].append(
+                        data, ignore_index=True)
+
+                    # shift column 'timestamp' to first position
+                    first_column = dfOut[result.tag].pop('timestamp')
+
+                    # insert column using insert(position, column_name, first_column) function
+                    dfOut[result.tag].insert(
+                        0, 'timestamp', first_column)
+
+                    out_name = result.tag.replace(".", "_") + '.csv'
+                    dfOut[result.tag].to_csv(out_name, index=False)
+
+            if not splitout:
+                # add the timestamp to the results
+                data['timestamp'] = date_time
+
+                # append the results to an empty data frame
+                dfOut = dfOut.append(data, ignore_index=True)
+
+                # shift column 'timestamp' to first position
+                first_column = dfOut.pop('timestamp')
+
+                # insert column using insert(position, column_name, first_column) function
+                dfOut.insert(0, 'timestamp', first_column)
+
+                # output data to CSV file
+                dfOut.to_csv(out, index=False)
+
+            # wait a number of seconds set in the config file
+            time.sleep(streamtime)
+
+    except KeyboardInterrupt:
+        pass
 
 
 def get_tags(ip):
