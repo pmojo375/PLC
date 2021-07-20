ef067a1339afbd96c7d4d7b1a7e232a4a514f561
Parker Mojsiejenko <pmojo375@gmail.com>
2021-07-20 09:48:05 -0400

    Added function calls to GUI

--- gui.py
+++ gui.py
@@ -1,15 +1,17 @@
 import PySimpleGUI as sg
 import plc
 
+ip = '192.168.1.1'
+
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [[sg.Text('Read a single PLC tag'), sg.B('Read Single Tag')],
-          [sg.Text('Write a single PLC tag'), sg.B('Write Single Tag')],
-          [sg.Text('Read multiple PLC tags'), sg.B('Read Multiple Tags')],
-          [sg.Text('Write multiple PLC tags'), sg.B('Write Multiple Tags')],
-          [sg.Text('Trend a PLC tag'), sg.B('Trend Single Tag')],
-          [sg.Text('Trend multiple PLC tags'), sg.B('Trend Multiple Tags')],
-          [sg.Text('Get PLC tags'), sg.B('Get Tags')],
+layout = [[sg.Text('Read a single PLC tag'), sg.B('Read Single Tag', key='Read Tag')],
+          [sg.Text('Write a single PLC tag'), sg.B('Write Single Tag', key='Write Tag')],
+          [sg.Text('Read multiple PLC tags'), sg.B('Read Multiple Tags', key='Read Tags')],
+          [sg.Text('Write multiple PLC tags'), sg.B('Write Multiple Tags', key='Write Tags')],
+          [sg.Text('Trend a PLC tag'), sg.B('Trend Single Tag', key='Trend Tag')],
+          [sg.Text('Trend multiple PLC tags'), sg.B('Trend Multiple Tags', key='Trend Tags')],
+          [sg.Text('Get PLC tags'), sg.B('Get Tags', key='Get Tags')],
           [sg.Button('Close')]]
 
 # Create the Window
@@ -21,13 +23,57 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Close':
         break
 
-    if event == 'Read Single Tag':
+    if event == 'Read Tag':
         tag = sg.popup_get_text('Enter tag name to read:')
 
-        print(tag)
+        print(f'Reading {tag}')
 
-    if event == 'Get_Tags':
-        tags = plc.get_tags('192.168.1.1')
+        plc.read_tag(ip, tag)
+
+    if event == 'Write Tag':
+        tag = sg.popup_get_text('Enter tag name to write to:')
+        value = plc.cast(sg.popup_get_text('Enter value to write:'))
+
+        print(f'Writing {value} to {tag}')
+
+        plc.write_tag(ip, tag, value)
+
+    if event == 'Read Tags':
+        tags = sg.popup_get_file('Locate CSV with tag names:', file_types = (('CSV','*.csv'),), initial_folder='./')
+
+        print(f'Filename {tags}')
+
+        print(f'Reading tags')
+
+        plc.read_tags_from_CSV(tags, ip, False)
+
+    if event == 'Write Tags':
+        tags = sg.popup_get_file('Locate CSV with tag names and values to write:', file_types = (('CSV','*.csv'),), initial_folder='./')
+
+        print(f'Filename {tags}')
+
+        print(f'Writing tags')
+
+        plc.write_tags_from_CSV(tags, ip)
+
+    if event == 'Trend Tag':
+        tag = sg.popup_get_text('Enter tag name to trend:')
+
+        print(f'Trending {tag}')
+
+        plc.trend_tag(ip, tag, False)
+
+    if event == 'Trend Tags':
+        tags = sg.popup_get_file('Locate CSV with tag names to trend:', file_types = (('CSV','*.csv'),), initial_folder='./')
+
+        print(f'Filename {tags}')
+
+        print(f'Trending tags')
+
+        plc.trend_tags(ip, tags, False, 1)
+
+    if event == 'Get Tags':
+        tags = plc.get_tags(ip)
         sg.popup_scrolled(tags, title='PLC Tags')
 
 window.close()
