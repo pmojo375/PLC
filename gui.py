e2dff75172b20778400e13a2bb92d6e2eecd7d83
Parker Mojsiejenko <pmojo375@gmail.com>
2021-07-20 08:06:04 -0400

    GUI work

--- gui.py
+++ gui.py
@@ -1,79 +1,33 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPalette
-import sys
+import PySimpleGUI as sg
+import plc
 
-# to add parameters to a function slot do this or use lambda:
-# btn.clicked.connect(functools.partial(greeting, 'World!'))
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [[sg.Text('Read a single PLC tag'), sg.B('Read Single Tag')],
+          [sg.Text('Write a single PLC tag'), sg.B('Write Single Tag')],
+          [sg.Text('Read multiple PLC tags'), sg.B('Read Multiple Tags')],
+          [sg.Text('Write multiple PLC tags'), sg.B('Write Multiple Tags')],
+          [sg.Text('Trend a PLC tag'), sg.B('Trend Single Tag')],
+          [sg.Text('Trend multiple PLC tags'), sg.B('Trend Multiple Tags')],
+          [sg.Text('Get PLC tags'), sg.B('Get Tags')],
+          [sg.Button('Close')]]
 
-class Window(QMainWindow):
-    """Main Window."""
-    def __init__(self):
-        """Initializer."""
-        super().__init__()
-        self.setWindowTitle('PLC Tag Read/Write')
-        #self.setFixedSize(235, 235)
-        self._centralWidget = QWidget(self)
-        self.setCentralWidget(self._centralWidget)
-        self._createMenu()
-        #self._createToolBar()
-        self._createStatusBar()
-        self.layout = QGridLayout()
-        self._centralWidget.setLayout(self.layout)
-        self._createReadButton()
-        self._createWriteButton()
-        self._createTable()
-        
+# Create the Window
+window = sg.Window('PLC', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Close':
+        break
 
-    def on_read_clicked(self):
-        alert = QMessageBox()
-        alert.setText('You clicked the read button!')
-        alert.exec()
-        
-    def on_write_clicked(self):
-        alert = QMessageBox()
-        alert.setText('You clicked the write button!')
-        alert.exec()
-        
-    def _createMenu(self):
-        self.menu = self.menuBar().addMenu("&Menu")
-        self.menu.addAction('&Exit', self.close)
+    if event == 'Read Single Tag':
+        tag = sg.popup_get_text('Enter tag name to read:')
 
-    def _createToolBar(self):
-        tools = QToolBar()
-        self.addToolBar(tools)
-        tools.addAction('Exit', self.close)
+        print(tag)
 
-    def _createStatusBar(self):
-        status = QStatusBar()
-        status.showMessage("I'm the Status Bar")
-        self.setStatusBar(status)
-        
-    def _createTable(self):
-        table = QTableWidget()
-        table.setRowCount(5)
-        table.setColumnCount(2)
-        table.setHorizontalHeaderLabels(['TAG', 'VALUE'])
-        table.cellChanged.connect(self.data_entered)
-        self.layout.addWidget(table, 1, 0, 1, 2)
-        
-    def _createReadButton(self):
-        read_button = QPushButton('Read Tag(s)')
-        self.layout.addWidget(read_button, 0, 1)
-        read_button.clicked.connect(self.on_read_clicked)
-        
-    def _createWriteButton(self):
-        write_button = QPushButton('Write Tag(s)')
-        self.layout.addWidget(write_button, 0, 0)
-        write_button.clicked.connect(self.on_write_clicked)
-        
-    def data_entered(row, column):
-        print(f'{row}, {column}')
+    if event == 'Get_Tags':
+        tags = plc.get_tags('192.168.1.1')
+        sg.popup_scrolled(tags, title='PLC Tags')
 
-        
-app = QApplication([])
-app.setStyle('Fusion')
-
-window = Window()
-window.show()
-app.exec()
+window.close()
